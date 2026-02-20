73927fc7db4e9ff31ac1399269d5ea998c884b5c
Satyam-Singh-x <singhsatyam.0912@gmail.com>
2026-02-20 12:18:36 +0530

    Update base_llm.py

--- core/base_llm.py
+++ core/base_llm.py
@@ -1,21 +1,19 @@
-import os
-from dotenv import load_dotenv
-from langchain_google_genai import ChatGoogleGenerativeAI
 import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
 
-load_dotenv()
 
+def get_llm(model_name: str = "gemini-1.5-flash", temperature: float = 0):
 
-def get_llm(model_name="gemini-2.5-flash", temperature=0.2):
-    api_key = st.secrets.get("GOOGLE_API_KEY")
-     if not api_key:
+    if "GOOGLE_API_KEY" not in st.secrets:
+        raise ValueError("GOOGLE_API_KEY missing in Streamlit secrets.")
+
+    api_key = st.secrets["GOOGLE_API_KEY"]
+
+    if not api_key:
         raise ValueError("GOOGLE_API_KEY is empty.")
+
     return ChatGoogleGenerativeAI(
         model=model_name,
         temperature=temperature,
         google_api_key=api_key
     )
-
-if __name__ == "__main__":
-    get_llm()
-    print("done")

e3d01bb02ee578df3f9d3171bc640affa9694196
Satyam-Singh-x <singhsatyam.0912@gmail.com>
2026-02-20 12:13:25 +0530

    Update base_llm.py

--- core/base_llm.py
+++ core/base_llm.py
@@ -1,15 +1,17 @@
 import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
+import streamlit as st
 
 load_dotenv()
 
 
 def get_llm(model_name="gemini-2.5-flash", temperature=0.2):
+    api_key = st.secrets.get("GOOGLE_API_KEY")
     return ChatGoogleGenerativeAI(
         model=model_name,
         temperature=temperature,
-        google_api_key=os.getenv("GOOGLE_API_KEY")
+        google_api_key=api_key
     )
 
 if __name__ == "__main__":
